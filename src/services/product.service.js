ccba03966e7926d9f6a64c9a0683fb6979271bc8
anton <a.gorbunov@id-east.ru>
2020-08-25 18:36:55 +0000

    Describe product service

---
+++ src/services/product.service.js
@@ -0,0 +1,57 @@
+import api from '../libs/api';
+import paginateProxy from '../helpers/paginate.proxy';
+
+const sortFields = ['name', 'price'];
+const sortDirections = ['asc', 'desc'];
+
+class ProductService {
+    validateParams(params = {}) {
+        console.log(params, 'params');
+        return Object.keys(params).reduce((acc = {}, key) => {
+            let value = params[key];
+
+            if (!value) return;
+
+            switch (key) {
+                case 'sort_field': 
+                    if (!sortFields.includes(value)) {
+                        console.warn('Pass sort value not be allowed');
+                        value = sortFields[0];
+                    }
+                    break;
+                case 'sort_direction':
+                    if (!sortDirections.includes(value)) {
+                        console.warn('Pass sort direction is not allowed.');
+                        value = sortDirections[0];
+                    }
+                    break;
+                case 'page':
+                    if (!Number.isInteger(value)) {
+                        console.warn('Page must be a number.');
+                        value = +value;
+                    }
+                    break;
+            }
+
+            acc[key] = value;
+
+            return acc;
+        }, {});
+    }
+
+    async list({
+        search = '',
+        sort_field = sortFields[0],
+        sort_direction = sortDirections[0],
+        page = 1
+    } = {}) {
+        console.log(search);
+        const { data: { products, ...paginate } } = await api.get('js-test-task/', {
+            params: this.validateParams({search, sort_field, sort_direction, page}),
+        });
+
+        return [products, paginate];
+    }
+}
+
+export default paginateProxy(ProductService, ['list']);
