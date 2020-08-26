074a3839c5f8025ae3986dd05f74e6047f54f55e
anton <a.gorbunov@id-east.ru>
2020-08-26 23:41:43 +0000

    product view page callback

--- src/pages/productView.js
+++ src/pages/productView.js
@@ -1,29 +1,27 @@
 import { Component } from '@/libs/core';
 
-function ProductView() {
-    const component = new Component({
-        app: '#root',
-    });
-
-    component.template = (async function (h) {
-        const selectProduct = this.vault.getItem('selectProduct');
-
-        return [
-            h('a', null, {
-                click: () => this.router.goto('/products')
-            }, ['Продукты']),
-            h('h1', null, null, [selectProduct.name]),
-            h('div', null, null, [selectProduct.price.toString()]),
-            h('img', {src: selectProduct.image, style: 'width: 300px'}, null, []),
-        ];
-    }).bind(component);
-
-    component.create();
-
-    return component;
-};
-
 export default {
     path: /products\/[0-9]$/gi,
-    clb: ProductView,
+    cb: function () {
+        const component = new Component({
+            app: '#root',
+        });
+    
+        component.template = (async function (h) {
+            const selectProduct = this.vault.getItem('selectProduct');
+    
+            return [
+                h('a', null, {
+                    click: () => this.router.goto('/products')
+                }, ['Продукты']),
+                h('h1', null, null, [selectProduct.name]),
+                h('div', null, null, [selectProduct.price.toString()]),
+                h('img', {src: selectProduct.image, style: 'width: 300px'}, null, []),
+            ];
+        }).bind(component);
+    
+        component.create();
+    
+        return component;
+    },
 }
